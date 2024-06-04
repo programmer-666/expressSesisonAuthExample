0d433942b00dcb2bdfcb7b46d8d43db1fd93ff7d
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-06-04 23:20:25 +0300

    added some comments

--- app.js
+++ app.js
@@ -5,66 +5,74 @@ import session from "express-session";
 import morgan from "morgan";
 
 const app = express();
-app.use(morgan("common"));
+app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const users = [{ klinux: "123" }, { root: "toor" }, { test: "test" }];
+// can be thought of as a database
 
 const authSession = session({
-  secret: "verysecret",
-  saveUninitialized: true,
-  resave: false,
-  cookie: {
-    maxAge: 60 * 1000,
-    secure: false,
-    httpOnly: true,
-  },
+        secret: "verysecret",
+        saveUninitialized: true,
+        resave: false,
+        cookie: {
+                maxAge: 60 * 1000,
+                secure: false,
+                httpOnly: true,
+        },
 });
+// this session holds users session
+// got 60 second expire time
+// if sessin time was expired then all operations inactive
 
 app.get("/", (_, res) => {
-  res.send("welcome, pls go auth for login").status(200);
+        res.send("welcome, pls go auth for login");
 });
 
-app.use("/auth", authSession);
+app.use("/auth", authSession); // authSession actively using here
 app.use("/auth", (req, res, next) => {
-  if (!req.session.authID) {
-    next();
-  } else {
-    res.send("You are already loged in.");
-  }
+        // middleware for check users session is created
+        if (!req.session.authID) {
+                next();
+        } else {
+                res.send("You are already loged in.");
+        }
 });
 app.post("/auth/", (req, res) => {
-  if (users.find((user) => req.body.username)[req.body.username] === req.body.password) {
-    req.session.authID = randomBytes(8).toString("hex");
-    req.session.usernInfo = [req.body.username, req.session.authID];
-    res.send({ auth: req.session.usernInfo });
-  } else {
-    res.send("User not found").status(404);
-  }
+        // if user data is correct then create a session for user
+        if (users.find((_) => req.body.username)[req.body.username] === req.body.password) {
+                req.session.authID = randomBytes(8).toString("hex");
+                req.session.usernInfo = [req.body.username, req.session.authID];
+                // res.send({ auth: req.session.usernInfo });
+                res.send().status(200);
+        } else {
+                res.send("Auth error").status(404);
+        }
 });
 
-app.use("/status", authSession);
+app.use("/status", authSession); // need session for use this url
 app.use("/status", (req, res, next) => {
-  if (req.session.authID) {
-    next();
-  } else {
-    res.send("Unauthorized").status(401);
-  }
+        if (req.session.authID) {
+                next();
+        } else {
+                // if session was not created then send 401
+                res.send("Unauthorized").status(401);
+        }
 });
 app.get("/status", (req, res) => {
-  res.send({ session: req.session.authID });
+        res.send({ session: req.session.authID });
 });
 
 app.use("/whoami", authSession);
 app.use("/whoami", (req, res, next) => {
-  if (req.session.authID) {
-    next();
-  } else {
-    res.send("Unauthorized").status(401);
-  }
+        if (req.session.authID) {
+                next();
+        } else {
+                res.send("Unauthorized").status(401);
+        }
 });
 app.get("/whoami", (req, res) => {
-  res.send(req.session.usernInfo[0]); // 0 is username
+        res.send(req.session.usernInfo[0]); // 0 is username
 });
 
 app.listen(80);
--- test_.sh
+++ test_.sh
@@ -3,4 +3,5 @@ curl -X POST http://localhost/auth \
 -c memory -d 'username='${args[0]}'&password='${args[1]} \
 -H "Content-Type: application/x-www-form-urlencoded" \
 --next -X GET -c memory http://localhost/status \
---next -X GET -c memory http://localhost/whoami
+--next -X GET -c memory http://localhost/whoami \
+&& rm memory
